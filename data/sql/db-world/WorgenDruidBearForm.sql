0ca912fe4b2489ced57d5a8bea82f73f9c3f0231
helldragonpz <46687151+helldragonpz@users.noreply.github.com>
2023-02-16 23:02:43 +0200

    WorgenDruidBearForm.sql
    This SQL File will fix the Worgen Bear Form Bug.

---
+++ data/sql/db-world/WorgenDruidBearForm.sql
@@ -0,0 +1,7 @@
+UPDATE `quest_template` SET `AllowableRaces` = 2056 WHERE (`ID` = 5923);
+UPDATE `quest_template` SET `AllowableRaces` = 2056 WHERE (`ID` = 5924);
+UPDATE `quest_template` SET `AllowableRaces` = 2056 WHERE (`ID` = 5925);
+UPDATE `quest_template` SET `AllowableRaces` = 2056 WHERE (`ID` = 5921);
+UPDATE `quest_template` SET `AllowableRaces` = 2056 WHERE (`ID` = 5929);
+UPDATE `quest_template` SET `AllowableRaces` = 2056 WHERE (`ID` = 5931);
+UPDATE `quest_template` SET `AllowableRaces` = 2056 WHERE (`ID` = 6001);
